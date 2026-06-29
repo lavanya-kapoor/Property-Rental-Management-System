659be73ca5ae047ea02edbc38b8b5559279d8e6d
lavanya-kapoor <132806198+lavanya-kapoor@users.noreply.github.com>
2026-06-29 17:52:50 -0400

    Add files via upload

---
+++ apex/f195840/application/pages/page_00006.sql
@@ -0,0 +1,321 @@
+prompt --application/pages/page_00006
+begin
+--   Manifest
+--     PAGE: 00006
+--   Manifest End
+wwv_flow_imp.component_begin (
+ p_version_yyyy_mm_dd=>'2026.03.30'
+,p_release=>'26.1.1'
+,p_default_workspace_id=>137183636994838360186
+,p_default_application_id=>195840
+,p_default_id_offset=>0
+,p_default_owner=>'WKSP_LAVANYAKAPOOR'
+);
+wwv_flow_imp_page.create_page(
+ p_id=>6
+,p_name=>'Open Maintenance'
+,p_alias=>'OPEN-MAINTENANCE'
+,p_step_title=>'Open Maintenance'
+,p_autocomplete_on_off=>'OFF'
+,p_step_template=>4072355960268175073
+,p_page_template_options=>'#DEFAULT#'
+,p_protection_level=>'C'
+,p_help_text=>wwv_flow_string.join(wwv_flow_t_varchar2(
+'<p>To find data enter a search term into the search dialog, or click on the column headings to limit the records returned.</p>',
+'',
+'<p>You can perform numerous functions by clicking the <strong>Actions</strong> button. This includes selecting the columns that are displayed / hidden and their display sequence, plus numerous data and format functions.  You can also define additiona'
+||'l views of the data using the chart, group by, and pivot options.</p>',
+'',
+'<p>If you want to save your customizations select report, or click download to unload the data. Enter you email address and time frame under subscription to be sent the data on a regular basis.<p>',
+'',
+'<p>For additional information click Help at the bottom of the Actions menu.</p> ',
+'',
+'<p>Click the <strong>Reset</strong> button to reset the interactive report back to the default settings.</p>'))
+,p_page_component_map=>'18'
+,p_created_on=>wwv_flow_imp.dz('20260417004151Z')
+,p_last_updated_on=>wwv_flow_imp.dz('20260417004203Z')
+,p_created_by=>'LK4803@G.RIT.EDU'
+,p_last_updated_by=>'LK4803@G.RIT.EDU'
+);
+wwv_flow_imp_page.create_page_plug(
+ p_id=>wwv_flow_imp.id(159005700199477466317)
+,p_plug_name=>'Breadcrumb'
+,p_static_id=>'breadcrumb'
+,p_region_template_options=>'#DEFAULT#:t-BreadcrumbRegion--useBreadcrumbTitle'
+,p_component_template_options=>'#DEFAULT#'
+,p_escape_on_http_output=>'N'
+,p_plug_template=>2531463326621247859
+,p_plug_display_sequence=>20
+,p_plug_display_point=>'REGION_POSITION_01'
+,p_plug_item_display_point=>'ABOVE'
+,p_menu_id=>wwv_flow_imp.id(159005431000110465621)
+,p_plug_source_type=>'NATIVE_BREADCRUMB'
+,p_menu_template_id=>4072363345357175094
+,p_plug_query_headings_type=>'COLON_DELMITED_LIST'
+,p_created_on=>wwv_flow_imp.dz('20260417004152Z')
+,p_updated_on=>wwv_flow_imp.dz('20260417004152Z')
+,p_created_by=>'LK4803@G.RIT.EDU'
+,p_updated_by=>'LK4803@G.RIT.EDU'
+);
+wwv_flow_imp_page.create_page_plug(
+ p_id=>wwv_flow_imp.id(159005694210985466203)
+,p_plug_name=>'Open Maintenance'
+,p_static_id=>'open-maintenance'
+,p_region_template_options=>'#DEFAULT#:t-IRR-region--hideHeader js-addHiddenHeadingRoleDesc'
+,p_escape_on_http_output=>'N'
+,p_plug_template=>2100526641005906379
+,p_plug_display_sequence=>10
+,p_plug_item_display_point=>'ABOVE'
+,p_query_type=>'TABLE'
+,p_query_table=>'V_OPEN_MAINTENANCE'
+,p_include_rowid_column=>false
+,p_plug_source_type=>'NATIVE_IR'
+,p_plug_query_headings_type=>'COLON_DELMITED_LIST'
+,p_prn_page_header=>'Open Maintenance'
+,p_ai_enabled=>false
+,p_created_on=>wwv_flow_imp.dz('20260417004152Z')
+,p_updated_on=>wwv_flow_imp.dz('20260417004203Z')
+,p_created_by=>'LK4803@G.RIT.EDU'
+,p_updated_by=>'LK4803@G.RIT.EDU'
+);
+wwv_flow_imp_page.create_worksheet(
+ p_id=>wwv_flow_imp.id(159005694328337466203)
+,p_max_row_count_message=>'The maximum row count for this report is #MAX_ROW_COUNT# rows.  Please apply a filter to reduce the number of records in your query.'
+,p_no_data_found_message=>'No data found.'
+,p_allow_save_rpt_public=>'Y'
+,p_pagination_type=>'ROWS_X_TO_Y'
+,p_pagination_display_pos=>'BOTTOM_RIGHT'
+,p_report_list_mode=>'TABS'
+,p_lazy_loading=>false
+,p_show_detail_link=>'N'
+,p_show_notify=>'Y'
+,p_download_formats=>'CSV:HTML:XLSX:PDF'
+,p_enable_mail_download=>'Y'
+,p_internal_uid=>159005694328337466203
+,p_created_on=>wwv_flow_imp.dz('20260417004152Z')
+,p_updated_on=>wwv_flow_imp.dz('20260417004203Z')
+,p_created_by=>'LK4803@G.RIT.EDU'
+,p_updated_by=>'LK4803@G.RIT.EDU'
+);
+wwv_flow_imp_page.create_worksheet_column(
+ p_id=>wwv_flow_imp.id(159005698615692466315)
+,p_db_column_name=>'ASSIGNEDSTAFF'
+,p_display_order=>10
+,p_column_identifier=>'J'
+,p_column_label=>'Assignedstaff'
+,p_column_type=>'STRING'
+,p_heading_alignment=>'LEFT'
+,p_tz_dependent=>'N'
+,p_use_as_row_header=>'N'
+,p_available_clientside=>'N'
+,p_created_on=>wwv_flow_imp.dz('20260417004152Z')
+,p_updated_on=>wwv_flow_imp.dz('20260417004203Z')
+,p_created_by=>'LK4803@G.RIT.EDU'
+,p_updated_by=>'LK4803@G.RIT.EDU'
+);
+wwv_flow_imp_page.create_worksheet_column(
+ p_id=>wwv_flow_imp.id(159005696214872466313)
+,p_db_column_name=>'DATESUBMITTED'
+,p_display_order=>4
+,p_column_identifier=>'D'
+,p_column_label=>'Datesubmitted'
+,p_column_type=>'DATE'
+,p_heading_alignment=>'LEFT'
+,p_tz_dependent=>'N'
+,p_use_as_row_header=>'N'
+,p_available_clientside=>'N'
+,p_created_on=>wwv_flow_imp.dz('20260417004152Z')
+,p_updated_on=>wwv_flow_imp.dz('20260417004203Z')
+,p_created_by=>'LK4803@G.RIT.EDU'
+,p_updated_by=>'LK4803@G.RIT.EDU'
+);
+wwv_flow_imp_page.create_worksheet_column(
+ p_id=>wwv_flow_imp.id(159005697008559466314)
+,p_db_column_name=>'DAYSOPEN'
+,p_display_order=>6
+,p_column_identifier=>'F'
+,p_column_label=>'Daysopen'
+,p_column_type=>'NUMBER'
+,p_heading_alignment=>'RIGHT'
+,p_column_alignment=>'RIGHT'
+,p_tz_dependent=>'N'
+,p_use_as_row_header=>'N'
+,p_available_clientside=>'N'
+,p_created_on=>wwv_flow_imp.dz('20260417004152Z')
+,p_updated_on=>wwv_flow_imp.dz('20260417004203Z')
+,p_created_by=>'LK4803@G.RIT.EDU'
+,p_updated_by=>'LK4803@G.RIT.EDU'
+);
+wwv_flow_imp_page.create_worksheet_column(
+ p_id=>wwv_flow_imp.id(159005696631758466313)
+,p_db_column_name=>'ISSUEDESCRIPTION'
+,p_display_order=>5
+,p_column_identifier=>'E'
+,p_column_label=>'Issuedescription'
+,p_allow_sorting=>'N'
+,p_allow_ctrl_breaks=>'N'
+,p_allow_aggregations=>'N'
+,p_allow_computations=>'N'
+,p_allow_charting=>'N'
+,p_allow_group_by=>'N'
+,p_allow_pivot=>'N'
+,p_column_type=>'CLOB'
+,p_heading_alignment=>'LEFT'
+,p_tz_dependent=>'N'
+,p_rpt_show_filter_lov=>'N'
+,p_use_as_row_header=>'N'
+,p_created_on=>wwv_flow_imp.dz('20260417004152Z')
+,p_updated_on=>wwv_flow_imp.dz('20260417004203Z')
+,p_created_by=>'LK4803@G.RIT.EDU'
+,p_updated_by=>'LK4803@G.RIT.EDU'
+);
+wwv_flow_imp_page.create_worksheet_column(
+ p_id=>wwv_flow_imp.id(159005695438717466312)
+,p_db_column_name=>'PRIORITY'
+,p_display_order=>2
+,p_column_identifier=>'B'
+,p_column_label=>'Priority'
+,p_column_type=>'STRING'
+,p_heading_alignment=>'LEFT'
+,p_tz_dependent=>'N'
+,p_use_as_row_header=>'N'
+,p_available_clientside=>'N'
+,p_created_on=>wwv_flow_imp.dz('20260417004152Z')
+,p_updated_on=>wwv_flow_imp.dz('20260417004203Z')
+,p_created_by=>'LK4803@G.RIT.EDU'
+,p_updated_by=>'LK4803@G.RIT.EDU'
+);
+wwv_flow_imp_page.create_worksheet_column(
+ p_id=>wwv_flow_imp.id(159005697829724466314)
+,p_db_column_name=>'PROPERTYADDRESS'
+,p_display_order=>8
+,p_column_identifier=>'H'
+,p_column_label=>'Propertyaddress'
+,p_column_type=>'STRING'
+,p_heading_alignment=>'LEFT'
+,p_tz_dependent=>'N'
+,p_use_as_row_header=>'N'
+,p_available_clientside=>'N'
+,p_created_on=>wwv_flow_imp.dz('20260417004152Z')
+,p_updated_on=>wwv_flow_imp.dz('20260417004203Z')
+,p_created_by=>'LK4803@G.RIT.EDU'
+,p_updated_by=>'LK4803@G.RIT.EDU'
+);
+wwv_flow_imp_page.create_worksheet_column(
+ p_id=>wwv_flow_imp.id(159005697471893466314)
+,p_db_column_name=>'PROPERTYID'
+,p_display_order=>7
+,p_column_identifier=>'G'
+,p_column_label=>'Propertyid'
+,p_column_type=>'NUMBER'
+,p_heading_alignment=>'RIGHT'
+,p_column_alignment=>'RIGHT'
+,p_tz_dependent=>'N'
+,p_use_as_row_header=>'N'
+,p_available_clientside=>'N'
+,p_created_on=>wwv_flow_imp.dz('20260417004152Z')
+,p_updated_on=>wwv_flow_imp.dz('20260417004203Z')
+,p_created_by=>'LK4803@G.RIT.EDU'
+,p_updated_by=>'LK4803@G.RIT.EDU'
+);
+wwv_flow_imp_page.create_worksheet_column(
+ p_id=>wwv_flow_imp.id(159005695011835466312)
+,p_db_column_name=>'REQUESTID'
+,p_display_order=>1
+,p_column_identifier=>'A'
+,p_column_label=>'Requestid'
+,p_column_type=>'NUMBER'
+,p_heading_alignment=>'RIGHT'
+,p_column_alignment=>'RIGHT'
+,p_tz_dependent=>'N'
+,p_use_as_row_header=>'N'
+,p_available_clientside=>'N'
+,p_created_on=>wwv_flow_imp.dz('20260417004152Z')
+,p_updated_on=>wwv_flow_imp.dz('20260417004203Z')
+,p_created_by=>'LK4803@G.RIT.EDU'
+,p_updated_by=>'LK4803@G.RIT.EDU'
+);
+wwv_flow_imp_page.create_worksheet_column(
+ p_id=>wwv_flow_imp.id(159005699005102466316)
+,p_db_column_name=>'SPECIALTY'
+,p_display_order=>11
+,p_column_identifier=>'K'
+,p_column_label=>'Specialty'
+,p_column_type=>'STRING'
+,p_heading_alignment=>'LEFT'
+,p_tz_dependent=>'N'
+,p_use_as_row_header=>'N'
+,p_available_clientside=>'N'
+,p_created_on=>wwv_flow_imp.dz('20260417004152Z')
+,p_updated_on=>wwv_flow_imp.dz('20260417004203Z')
+,p_created_by=>'LK4803@G.RIT.EDU'
+,p_updated_by=>'LK4803@G.RIT.EDU'
+);
+wwv_flow_imp_page.create_worksheet_column(
+ p_id=>wwv_flow_imp.id(159005695806068466312)
+,p_db_column_name=>'STATUS'
+,p_display_order=>3
+,p_column_identifier=>'C'
+,p_column_label=>'Status'
+,p_column_type=>'STRING'
+,p_heading_alignment=>'LEFT'
+,p_tz_dependent=>'N'
+,p_use_as_row_header=>'N'
+,p_available_clientside=>'N'
+,p_created_on=>wwv_flow_imp.dz('20260417004152Z')
+,p_updated_on=>wwv_flow_imp.dz('20260417004203Z')
+,p_created_by=>'LK4803@G.RIT.EDU'
+,p_updated_by=>'LK4803@G.RIT.EDU'
+);
+wwv_flow_imp_page.create_worksheet_column(
+ p_id=>wwv_flow_imp.id(159005698232289466315)
+,p_db_column_name=>'TENANTNAME'
+,p_display_order=>9
+,p_column_identifier=>'I'
+,p_column_label=>'Tenantname'
+,p_column_type=>'STRING'
+,p_heading_alignment=>'LEFT'
+,p_tz_dependent=>'N'
+,p_use_as_row_header=>'N'
+,p_available_clientside=>'N'
+,p_created_on=>wwv_flow_imp.dz('20260417004152Z')
+,p_updated_on=>wwv_flow_imp.dz('20260417004203Z')
+,p_created_by=>'LK4803@G.RIT.EDU'
+,p_updated_by=>'LK4803@G.RIT.EDU'
+);
+wwv_flow_imp_page.create_worksheet_rpt(
+ p_id=>wwv_flow_imp.id(159005854851639467341)
+,p_application_user=>'APXWS_DEFAULT'
+,p_report_seq=>10
+,p_report_alias=>'1590058549'
+,p_status=>'PUBLIC'
+,p_is_default=>'Y'
+,p_report_columns=>'PRIORITY:STATUS:DATESUBMITTED:ISSUEDESCRIPTION:DAYSOPEN:PROPERTYADDRESS:TENANTNAME:ASSIGNEDSTAFF:SPECIALTY'
+,p_sort_column_1=>'PRIORITY'
+,p_sort_direction_1=>'ASC'
+,p_created_on=>wwv_flow_imp.dz('20260417004203Z')
+,p_updated_on=>wwv_flow_imp.dz('20260417004203Z')
+,p_created_by=>'LK4803@G.RIT.EDU'
+,p_updated_by=>'LK4803@G.RIT.EDU'
+);
+wwv_flow_imp_page.create_page_button(
+ p_id=>wwv_flow_imp.id(159005699491821466316)
+,p_button_sequence=>10
+,p_button_plug_id=>wwv_flow_imp.id(159005694210985466203)
+,p_button_name=>'RESET_REPORT'
+,p_static_id=>'reset-report'
+,p_button_action=>'REDIRECT_PAGE'
+,p_button_template_options=>'t-Button--iconLeft'
+,p_button_template_id=>2082829544945815391
+,p_button_image_alt=>'Reset'
+,p_button_position=>'RIGHT_OF_IR_SEARCH_BAR'
+,p_button_redirect_url=>'f?p=&APP_ID.:6:&APP_SESSION.::&DEBUG.:RR::'
+,p_icon_css_classes=>'fa-undo-alt'
+,p_created_on=>wwv_flow_imp.dz('20260417004152Z')
+,p_updated_on=>wwv_flow_imp.dz('20260417004152Z')
+,p_created_by=>'LK4803@G.RIT.EDU'
+,p_updated_by=>'LK4803@G.RIT.EDU'
+);
+wwv_flow_imp.component_end;
+end;
+/
---
+++ apex/f195840/application/shared_components/security/authorizations/administration_rights.sql
@@ -0,0 +1,31 @@
+prompt --application/shared_components/security/authorizations/administration_rights
+begin
+--   Manifest
+--     SECURITY SCHEME: Administration Rights
+--   Manifest End
+wwv_flow_imp.component_begin (
+ p_version_yyyy_mm_dd=>'2026.03.30'
+,p_release=>'26.1.1'
+,p_default_workspace_id=>137183636994838360186
+,p_default_application_id=>195840
+,p_default_id_offset=>0
+,p_default_owner=>'WKSP_LAVANYAKAPOOR'
+);
+wwv_flow_imp_shared.create_security_scheme(
+ p_id=>wwv_flow_imp.id(159005436444244465659)
+,p_name=>'Administration Rights'
+,p_static_id=>'administration-rights'
+,p_scheme_type=>'NATIVE_FUNCTION_BODY'
+,p_attributes=>wwv_flow_t_plugin_attributes(wwv_flow_t_varchar2(
+  'plsql_function_body', 'return true;')).to_clob
+,p_error_message=>'Insufficient privileges, user is not an Administrator'
+,p_version_scn=>'15758026140175'
+,p_caching=>'BY_USER_BY_PAGE_VIEW'
+,p_created_on=>wwv_flow_imp.dz('20260417004146Z')
+,p_updated_on=>wwv_flow_imp.dz('20260417004146Z')
+,p_created_by=>'LK4803@G.RIT.EDU'
+,p_updated_by=>'LK4803@G.RIT.EDU'
+);
+wwv_flow_imp.component_end;
+end;
+/
